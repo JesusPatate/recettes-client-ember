15875e5035e285f2871691028f30f2c063d3d2c7
JesusPatate <gg.jesuspatate@orange.fr>
2021-01-31 21:14:23 +0100

    Fix source field

--- app/components/recipe/details.js
+++ app/components/recipe/details.js
@@ -2,12 +2,22 @@ import Component from '@glimmer/component';
 
 export default class RecipeDetailsComponent extends Component {
 
+    get source() {
+        let source = this.args.recipe.source;
+        
+        if (this.isUrl(source)) {
+            return '<a href="' + source + '" target="_blank" rel="noopener noreferrer">' + this.getDomain(source) +'</a>'
+        }
+        
+        return source;
+    }
+
     isUrl(string) {
-        let re = /^https?:\/\//
+        let re = /^https?:\/\//;
         return re.test(string)
     }
 
     getDomain(url) {
-        return url.split('/')[1];
+        return url.split('/')[2];
     }
 }
--- app/helpers/getdomainurl.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-function getdomainurl([string]) {
-  return string.split('/')[2];
-}
-
-export default helper(getdomainurl);
